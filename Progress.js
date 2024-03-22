509f2cda7567dbb72958346ef6e9429d77d79d85
Genisson Emilio <120515204+Genisson0@users.noreply.github.com>
2024-03-22 19:46:10 -0300

    Update Progress.js

--- Progress.js
+++ Progress.js
@@ -7,54 +7,37 @@ class Progress {
     this.saveFileKey = 'PizzaLegends_SaveFile1';
   }
   
-  async save() {
-    try {
-      const response = await fetch('http://localhost:3000/save_player_data', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          mapId: this.mapId,
-          startingHeroX: this.startingHeroX,
-          startingHeroY: this.startingHeroY,
-          startingHeroDirection: this.startingHeroDirection,
-          playerState: {
-            pizzas: playerState.pizzas,
-            lineup: playerState.lineup,
-            items: playerState.items,
-            storyFlags: playerState.storyFlags
-          }
-        })
-      });
-      const result = await response.json();
-      console.log('resultado: ', result);
-    } catch (error) {
-      console.error('Erro ao salvar dados:', error);
-    }
+  save() {
+    window.localStorage.setItem(this.saveFileKey, JSON.stringify({
+      mapId: this.mapId,
+      startingHeroX: this.startingHeroX,
+      startingHeroY: this.startingHeroX,
+      startingHeroDirection: this.startingHeroDirection,
+      playerState: {
+        pizzas: playerState.pizzas,
+        lineup: playerState.lineup,
+        items: playerState.items,
+        storyFlags: playerState.storyFlags
+      }
+    }));
   }
   
-  async getSaveFile() {
-    const response = await fetch('http://localhost:3000/player_data');
-    const data = await response.json();
-    return data ? data : null
+  getSaveFile() {
+    const file = window.localStorage.getItem(this.saveFileKey);
+    return file ? JSON.parse(file) : null
   }
   
   load() {
-    try {
-      const data = this.getSaveFile();
-      if (data) {
-        this.mapId = data.map_id;
-        this.startingHeroX = data.starting_hero_X;
-        this.startingHeroY = data.starting_hero_Y;
-        this.startingHeroDirection = data.starting_hero_direction;
-        playerState.pizzas = data.pizzas;
-        playerState.lineup = data.lineup;
-        playerState.items = data.items;
-        playerState.storyFlags = data.story_flags;
-      }
-    } catch (error) {
-      console.error('Erro ao carregar dados:', error);
+    const file = this.getSaveFile();
+    if (file) {
+      this.mapId = file.mapId;
+      this.startingHeroX = file.startingHeroX;
+      this.startingHeroY = file.startingHeroY;
+      this.startingHeroDirection = file.startingHeroDirection;
+      Object.keys(file.playerState).forEach(key => {
+        playerState[key] = file.playerState[key];
+      })
     }
   }
+  
 }
