e2d4f7542e2feab057a9a32ee54b97ce73e4a0bf
Genisson Emilio <esgenisson@gmail.com>
2025-02-26 15:03:02 -0300

    update
    i'm crazy

--- Progress.js
+++ Progress.js
@@ -1,6 +1,6 @@
 class Progress {
   constructor() {
-    this.mapId = "Kitchen";
+    this.mapId = "DemoRoom";
     this.startingHeroX = 0;
     this.startingHeroY = 0;
     this.startingHeroDirection = "down";
